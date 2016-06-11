abeb25900f6f9ff40b6824d10679655736d34723
juanlove <juan.a.urena@gmail.com>
2016-06-10 21:22:52 -0700

    new images and color changes, fixed full navbar color

--- js/custom.js
+++ js/custom.js
@@ -83,8 +83,8 @@ $(function(){
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg",
-       "images/home-bg-slideshow2.jpg",
+       "images/background1.png",
+       "images/background1alt.png",
         ],  {duration: 2000, fade: 750});
     });
   })
