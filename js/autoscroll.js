66d8ee4c72ee45aa59b2c870c131024b61e55963
Juan Urena <Juan Urena>
2017-05-15 13:58:19 -0700

    email change

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -1,12 +1,19 @@
-$("html, body").animate({ scrollTop: $(document).height() }, 15000);
-setTimeout(function() {
-   $('html, body').animate({scrollTop:0}, 4000);
-},4000);
-setInterval(function(){
-     // 4000 - it will take 4 secound in total from the top of the page to the bottom
-$("html, body").animate({ scrollTop: $(document).height() }, 4000);
-setTimeout(function() {
-   $('html, body').animate({scrollTop:0}, 4000);
-},4000);
-
-},8000);
+$("html, body").animate({
+    scrollTop: $(document).height()
+}, 15000);
+setTimeout(function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 4000);
+}, 4000);
+setInterval(function () {
+    // 4000 - it will take 4 secound in total from the top of the page to the bottom
+    $("html, body").animate({
+        scrollTop: $(document).height()
+    }, 4000);
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 4000);
+    }, 4000);
+}, 8000);
